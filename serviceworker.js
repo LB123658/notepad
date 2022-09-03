b988f380c2bab7e49dfb9b1b75843e1b36d700d6
lb123658 <79328251+LB123658@users.noreply.github.com>
2022-09-02 21:59:56 -0500

    Create serviceworker.js

---
+++ serviceworker.js
@@ -0,0 +1,22 @@
+var staticCacheName = "notepad";
+
+self.addEventListener("install", function (e) {
+e.waitUntil(
+	caches.open(staticCacheName).then(function (cache) {
+	return cache.addAll(["/"]);
+	})
+);
+});
+
+self.addEventListener("fetch", function (event) {
+console.log(event.request.url);
+
+event.respondWith(
+	caches.match(event.request).then(function (response) {
+	return response || fetch(event.request);
+	})
+);
+});
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
